17e7a889c6df974c33e893b927937445ed29eb83
LevanB <levanb@inceptionxr.com>
2022-06-20 14:58:10 +0300

    Mula components

---
+++ Assets/_ROOT/_LB/_RAFTS/Mula/Ads.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using RAFTS;
+using UnityEngine;
+
+public class Ads : MonoBehaviour
+{
+    public Step Interstitial 
+    => 
+        new Step(nameof(Interstitial),
+                 "",
+                 s =>
+                 {
+                     
+                 });
+                 
+    public Step Rewarded 
+    => 
+        new Step(nameof(Rewarded),
+                 "",
+                 s =>
+                 {
+                     
+                 });
+                 
+    public Step Banner 
+    => 
+        new Step(nameof(Banner),
+                 "",
+                 s =>
+                 {
+                     
+                 });
+    
+}
