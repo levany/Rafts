ce90328144475603d74b473df858d6eea5b6d05d
LevanB <LevanB@inceptionxr.com>
2022-06-26 18:48:44 +0300

    post build scripts

--- Scripts/PostBuild.sh
+++ Scripts/PostBuild.sh
@@ -1,4 +1,15 @@
 echo "LB - ."
 echo "LB - POST BUILD SHELL SCRIPT"
+
+echo "LB - PWD : "
 pwd
+
+echo "LB - ===================================="
+echo "LB - ENVIRONMENT : "
+env
+echo "LB - ===================================="
+
+echo "LB - Fastlane : "
 fastlane -v
+
+echo "LB - ."
--- Scripts/PreBuild.sh
+++ Scripts/PreBuild.sh
@@ -1,3 +1,7 @@
 echo "LB - ."
 echo "LB - PRE BUILD SHELL SCRIPT"
+echo "LB - PWD : "
 pwd
+echo "LB - ENVIRONMENT : "
+env
+echo "LB - ."
