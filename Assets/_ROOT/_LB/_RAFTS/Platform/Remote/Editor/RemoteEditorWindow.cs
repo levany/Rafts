d9281325929fbda6f717365b4120d046dac867c1
LevanB <LevanB@inceptionxr.com>
2022-07-09 15:18:15 +0300

    added panda, chick and duck

---
+++ Assets/_ROOT/_LB/_RAFTS/Platform/Remote/Editor/RemoteEditorWindow.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class RemoteEditorWindow : EditorWindow
+{
+    [MenuItem("Rafts/Remote")]
+    public static void ShowExample()
+    {
+        RemoteEditorWindow wnd = GetWindow<RemoteEditorWindow>();
+        wnd.titleContent = new GUIContent("Remote");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+        
+    }
+}
