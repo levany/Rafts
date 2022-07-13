3ff7a4f1adbe3d620c407318f4c9a79b72c1f8ff
LevanB <LevanB@inceptionxr.com>
2022-07-13 21:26:18 +0300

    added unity recorder, moved some folders

---
+++ Assets/_ROOT/_LB/_RAFTS/Elements/PresskitElements.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using Object = UnityEngine.Object;
+using System.Threading.Tasks;
+using UnityEditor.Recorder;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif // UNITY_EDITOR
+
+public class PresskitElements : MonoBehaviour
+{
+    #if UNITY_EDITOR
+    
+    [MenuItem("Rafts/Elements/Presskit/EditorScreenshot")]
+     private static void TakeEditorScreenshot()
+     {
+         var dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Presskit/";
+         Directory.CreateDirectory(dir);
+
+         var filename = "EditorScreenshot.png";
+         var path = dir + filename;
+         
+     }
+    
+     [MenuItem("Rafts/Elements/Presskit/Screenshot")]
+     private static void TakeScreenshot()
+     {
+         var dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Presskit/";
+         Directory.CreateDirectory(dir);
+
+         var filename = "Screenshot.png";
+         var path = dir + filename;
+         
+         ScreenCapture.CaptureScreenshot(path);
+         
+         
+         Debug.Log($"Screenshot saved: <b>{path}</b>");   
+     }
+     
+     [MenuItem("Rafts/Elements/Presskit/Record")]
+     private static async Task RecordVideo()
+     {
+         Debug.Log("Start");
+         
+         await Task.Delay(3000);
+         
+         Debug.Log("End");
+     }
+
+    
+    #endif // UNITY_EDITOR
+}
